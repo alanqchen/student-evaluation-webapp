94f577f067cd8691c807678725af54c0aa5178b0
Alan Chen <alanchen1908@gmail.com>
2022-04-13 19:12:00 -0400

    Test database config connection

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'users/new'
+  get '/signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/migrate/20220412034502_create_users.rb
+++ db/migrate/20220412034502_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :email, null: false
+      t.string :email, null: false, unique: true
       t.boolean :instructor, null: false
       t.boolean :admin, null: false
       t.boolean :approver, null: false
