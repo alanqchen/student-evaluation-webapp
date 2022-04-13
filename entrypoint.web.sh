031f18161ce7a44562455a20c5dc4f2b5da6d877
Alan Chen <alanchen1908@gmail.com>
2022-04-13 02:25:57 -0400

    Add removing pre-existing server.pid in heroku entrypoint

--- entrypoint.web.sh
+++ entrypoint.web.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "Running entrypoint.sh"
 set -e
+
+echo "Remove any server.pid"
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /usr/src/app/tmp/pids/server.pid
+
 # For prod we bundle inside the dockerfile, not the entrypoint
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
