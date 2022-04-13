bb30e5071a9f25f8ca4856dd783f1cabd217bafa
Alan Chen <alanchen1908@gmail.com>
2022-04-13 02:32:30 -0400

    Fix heroku tmp/pids path

--- entrypoint.web.sh
+++ entrypoint.web.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Remove any server.pid"
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /usr/src/app/tmp/pids/server.pid
+rm -f /eval-webapp/tmp/pids/server.pid
 
 # For prod we bundle inside the dockerfile, not the entrypoint
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
