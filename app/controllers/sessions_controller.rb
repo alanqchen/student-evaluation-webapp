89138020b1b0bacaf53880cf22d48b1d3162b762
Alan Chen <alanchen1908@gmail.com>
2022-04-14 05:52:56 -0400

    Add basic user validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)&.authenticate params[:session][:password]
+    if user
+      # TODO
+      flash.now[:success] = {title: 'Success!', message: ' Valid email/password combination'}
+      render turbo_stream: turbo_stream.replace("flash_alert", partial: "partials/flash", locals: { flash: flash })
+    else
+      # TODO
+      flash.now[:danger] = {title: 'Error!', message: ' Invalid email/password combination'}
+      render turbo_stream: turbo_stream.replace("flash_alert", partial: "partials/flash", locals: { flash: flash })
+    end
   end
 
   def destroy
