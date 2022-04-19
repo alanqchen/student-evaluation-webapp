b1bb250e2b3b5993826b136de6353190c18fd1dd
Alan Chen <alanchen1908@gmail.com>
2022-04-19 16:19:23 -0400

    Change prod to use redis adapter for cable

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./flowbite-1.4.1-patched"
+import "./flowbite-1.4.1-patched.js"
