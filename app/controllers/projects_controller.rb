794b284d67fb293a48d127b4f6ef4b7b44df9749
jcalentine <calentine.4@gmail.com>
2022-04-20 02:47:35 -0400

    rubocop fixed errors

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,13 @@
 class ProjectsController < ApplicationController
-  
+
   def show
     @project = Project.find params[:id]
   end
 
   def new
-  
+
   end
-  
+
   #def index
    # @projects = Project.all
   #end
