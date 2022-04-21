12c3fe79b62ac518b73d8ec6456344c1bf9e2cde
Blake Whitman <whitman.124@osu.edu>
2022-04-21 12:17:38 -0400

    added test cases for controllers following rspec guidelines, added updates to evaluation controller

---
+++ spec/tests/courses_controller_test.rb
@@ -0,0 +1,40 @@
+require 'rspec/autorun'
+
+describe CoursesController do
+    let!(:course) {CoursesController.new}
+
+    it "should get index" do
+        get courses_url
+        assert_response :redirect
+    end
+    
+    
+    it "should show course" do
+        get course_url(@course)
+        assert_response :redirect
+    end
+    
+    it "should get edit" do
+        get edit_course_url(@course)
+        assert_response :redirect
+    end
+    
+    it "should not save course without user_id" do 
+        course = Course.new
+        assert_not course.save
+    end 
+    
+    it "should report error" do 
+        assert_raises(NameError) do
+          some_undefined_variable 
+        end 
+    end 
+    
+    it "should destroy course" do
+        assert_difference('Course.count', -1) do
+          delete course_url(@course)
+        end
+    
+        assert_redirected_to courses_url
+    end
+end
---
+++ spec/tests/evaluations_controller_test.rb
@@ -0,0 +1,26 @@
+require 'rspec/autorun'
+
+describe EvaluationsController do
+    let!(:evaluate) {EvaluationsController.new}
+
+    it "should get index" do
+        get eval_url
+        assert_response :redirect
+    end
+    
+    
+    it "should get new" do
+        get new_eval_url
+        assert_response :redirect
+    end
+    
+    it "should get edit" do
+        get edit_eval_url(@evaluate)
+        assert_response :redirect
+    end
+    
+    it "should show evaluation" do
+        get eval_url(@evaluate)
+        assert_response :redirect
+      end
+end
