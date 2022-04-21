2c8ec3ba68af6b9e1af29322131d5b08b6c79b75
jcalentine <calentine.4@gmail.com>
2022-04-20 23:51:17 -0400

    added create new project functionality to view, modified routes to access new project page, modified project controller to be able to add projects

--- config/routes.rb
+++ config/routes.rb
@@ -34,8 +34,11 @@ Rails.application.routes.draw do
     resources :dashboards, path: 'dashboard'
   end
   resources :users
+
   resources :courses
   resources :projects
+  get 'projects/new'
+
   resources :password_resets, only: [:new, :create, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
