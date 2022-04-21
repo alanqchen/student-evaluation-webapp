6c4587bd95ead2ab99835d6bb937f9943dcce717
Blake Whitman <whitman.124@osu.edu>
2022-04-21 12:57:02 -0400

    fix rubocop error messages part 3

--- spec/tests/courses_controller_test.rb
+++ spec/tests/courses_controller_test.rb
@@ -7,29 +7,23 @@ describe CoursesController do
         get courses_url
         assert_response :redirect
     end
-    
-    
     it "should show course" do
         get course_url(@course)
         assert_response :redirect
     end
-    
     it "should get edit" do
         get edit_course_url(@course)
         assert_response :redirect
     end
-    
     it "should not save course without user_id" do
         course = Course.new
         assert_not course.save
     end
-    
     it "should report error" do
         assert_raises NameError do
           some_undefined_variable
         end
     end
-
     it "should destroy course" do
         assert_difference('Course.count', -1) do
           delete course_url(@course)
